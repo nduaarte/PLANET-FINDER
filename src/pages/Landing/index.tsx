58e08f5cec09a33e29a919114fc7508e3052dba8
Nycollas <nycollas.duaarte@hotmail.com>
2020-11-03 21:51:37 -0300

    finish-landing-page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import img1 from '../../images/landingImages/bobafet.jpg';
+import img2 from '../../images/landingImages/darthmaul.jpg';
+import img3 from '../../images/landingImages/darthvader.jpg';
+import img4 from '../../images/landingImages/darthvader2.jpg';
+import img5 from '../../images/landingImages/kyloren.jpg';
+import img6 from '../../images/landingImages/luke.jpg';
+import img7 from '../../images/landingImages/yoda.jpg';
+
+import { Container } from './styles';
 
 const Landing: React.FC = () => {
-  return <div />;
+  const imagesArr = [img1, img2, img3, img4, img5, img6, img7];
+  const [image, setImage] = useState(img1);
+  let count = 0;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count > 6)
+        count = 0;
+      setImage(imagesArr[count]);
+
+      count++;
+
+      return () => clearInterval(interval);
+    }, 5000)
+  }, []);
+
+  return (
+    <Container>
+      <img src={image} alt="image" />
+      
+      <div>
+        <h1>PLANET FINDER</h1>
+
+        <p>
+          Aqui é possível consultar todos os planetas da galáxia e ver quais espécies e
+          criaturas habitam cada planeta.
+      </p>
+
+        <button type="button">Começar</button>
+      </div>
+    </Container>
+  );
 }
 
 export default Landing;
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+interface Props {
+  img: string;
+}
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+
+  img {
+    display: flex;
+    width: 100%;
+    max-height: 100vh;
+
+  } 
+
+  div {
+    display: flex;
+    margin-top: 40px;
+
+    position: absolute;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h1 {
+    display: flex;
+    font-size: 70px;
+    color: var(--yellow);
+  }
+
+  p {
+    display: flex;
+    color: var(--white);
+    font-size: 18px;
+  }
+
+  button {
+    display: flex;
+    background: var(--yellow2);
+    color: var(--white);
+    margin-top: 50px;
+
+    font-size: 22px;
+    padding: 10px 15px;
+    border-radius: 5px;
+
+    :hover {
+      color: var(--lightgrey);
+      background: var(--yellow);
+      cursor: pointer;
+    }
+    
+    transition: 200ms;
+  }
+`;
